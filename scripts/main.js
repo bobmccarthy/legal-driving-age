99b41cd3448f6b3c992848398c7191df8302a650
bob <bobmccarthyatx@gmail.com>
2015-10-20 13:44:12 -0500

    all terneries

--- scripts/main.js
+++ scripts/main.js
@@ -10,13 +10,10 @@ var $output = $('.output');
 $form.on('submit', function(e){
 	e.preventDefault();
 	var x;
-	x = ($input.val()>=16 ? 'Drive up a storm!' : 'Go home');
-	if ($input.val()<1){
-		x = 'Please enter a valid age';
-	}
-	if ($input.val()>99){
-		x = 'Get off the road, Geezer';
-	}
+	
+	x = ($input.val()<1 ? 'Please enter a valid age' 
+		: $input.val()>99 ? 'Get off the road, Geezer!' 
+		: $input.val()>=16 ? 'Drive up a storm!': 'go home stupid');
 	$output.html(x);
 	$input.val('');
 })
