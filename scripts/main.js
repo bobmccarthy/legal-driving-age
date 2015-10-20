059a070ef2108983b58d15aecfab3113b29da547
bob <bobmccarthyatx@gmail.com>
2015-10-20 13:31:30 -0500

    working

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+'use strict';
+var $ = require ('jquery');
+window.$ = require('jquery');
+window.jQuery = $;
+
+var $form = $('form');
+var $input = $('input');
+var $output = $('.output');
+
+$form.on('submit', function(e){
+	e.preventDefault();
+	var x;
+	x = ($input.val()>=16 ? 'Drive up a storm!' : 'Go home');
+	if ($input.val()<1){
+		x = 'Please enter a valid age';
+	}
+	$output.html(x);
+	$input.val('');
+})
